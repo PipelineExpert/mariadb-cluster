629adbb9970f668e7c7dd9f36cd2dcd2b8f5cfad
ubuntu <ubuntu@vernoncompany.com>
2016-03-17 09:48:16 -0500

    added scripts to migrate data from mariadb to galera

---
+++ get_user.py
@@ -0,0 +1,32 @@
+#!/bin/python
+from subprocess import Popen, PIPE
+import sys
+users=( Popen(['/bin/sh', '-c',
+			"mysql -h10.1.1.49 -u " + sys.argv[1] + " -p" + sys.argv[2] + " -Be 'use mysql;select * from user;'"
+			], stdout=PIPE) )
+index=0
+mysql_users=[]
+
+for i in iter(users.stdout.readline, b''):
+	if index == 0:
+		i=i.replace("\n",'')
+		i=i.replace("\n",'')
+		i = i.replace( "\t", ",")
+		headers = i
+		index+=1
+	else:
+		i = i.replace( "\t", "','")
+		i = "'" + i + "'"
+		mysql_users+=[i]
+sql = "INSERT INTO mysql.user (" + headers + ") VALUES "
+index = 0
+for row in mysql_users:
+	if index == 0:
+		sql += "(" + row + ")"
+		index += 1
+	else:
+		sql += ",(" + row + ")"
+sql += ";"
+file = open("/tmp/users.sql", "w")
+file.write(sql)
+file.close()
